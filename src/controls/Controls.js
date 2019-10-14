410cc0045ed4de938e0075dbffcecf9bbfbc5748
David.Johnson <davidt.johnson75@yahoo.com>
2019-10-14 12:15:13 -0500

    MVP met

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -4,11 +4,11 @@ const Controls = props => {
   const { locked, closed, toggleLocked, toggleClosed } = props;
 
   return (
-    <div className="controls panel">
+    <div className="Control panel" data-testid="rules">
       <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
         {locked ? 'Unlock Gate' : 'Lock Gate'}
       </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
+      <button data-testid="Switch" disabled={locked} onClick={toggleClosed} className="toggle-btn">
         {closed ? 'Open Gate' : 'Close Gate'}
       </button>
     </div>
